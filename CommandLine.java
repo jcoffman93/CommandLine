8acd077b9bba228779a434e8b7c5f3b823dd8213
jcoffman <jcoffman93@yahoo.com>
2013-09-28 21:03:27 -0400

    Nothing changed really.

--- CommandLine.java
+++ CommandLine.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 class CommandLine {
 
 	static class Parser {
 		private String[] commandList;
 		private final int COMMAND_LIST_SIZE;
+		private Scanner myScanner;
 
 		private Parser (String[] commands) {
 			COMMAND_LIST_SIZE = commands.length;
@@ -10,12 +13,11 @@ class CommandLine {
 			for(int i = 0; i < COMMAND_LIST_SIZE; i++) {
 				commandList[i] = commands[i];
 			}
-
-			
+			myScanner = new Scanner(System.in);			
 		}
 
-		private void parse (String commandString) {
-			String[] commands = commandString.split(" *\\| *");
+		private void parseLine () {
+			String[] commands = myScanner.nextLine().split(" *\\| *");
 			for(int i =0; i<commands.length; i++) System.out.println(commands[i]);
 		}
 
@@ -24,6 +26,9 @@ class CommandLine {
 	public static void main (String[] args) {
 		String[] commands = {"hello", "there", "world"};
 		CommandLine.Parser myParser = new CommandLine.Parser(commands);
-		myParser.parse("hello|there | world > blah.pdf");
+		while(true) {
+			System.out.print("> ");
+			myParser.parseLine();
+		}
 	}
 }
